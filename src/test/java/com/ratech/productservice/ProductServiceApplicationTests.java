764f4aa7e5db7f471a8e381bcaa8b72de63845a8
Ramkumar <rockeyramsaran@gmail.com>
2025-06-30 22:59:11 +0530

    Project Initial Setup

---
+++ src/test/java/com/ratech/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratech.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
